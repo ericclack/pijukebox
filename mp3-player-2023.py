5265478037a0e2b450db11052b86a2be61dcfc8f
Eric Clack <eric@bn7.net>
2023-05-01 13:31:44 +0100

    More buttons

--- mp3-player-2023.py
+++ mp3-player-2023.py
@@ -4,10 +4,7 @@ import RPi.GPIO as IO
 
 PLAYER = "/usr/bin/mpc"
 LAST_EVENT = 0
-BUTTON_BOUNCE_GAP = 0.1 #seconds
-
-IO.setmode(IO.BOARD)
-IO.setup(10, IO.IN, pull_up_down=IO.PUD_DOWN)
+BUTTON_BOUNCE_GAP = 0.5 #seconds
 
 def mpc(command):
     os.system('mpc %s' % command)
@@ -18,30 +15,42 @@ def mpc_add_all_to_playlist():
 def set_up_playlist():
     mpc('clear')
     mpc_add_all_to_playlist()
-    mpc('random off')
+    mpc('random on')
     mpc('repeat on')
-    mpc('volume 79')
+    mpc('volume 70')
 
-def play_next_mp3():
-    mpc('play')
-    mpc('next')
-    
 def button_bounce():
     print(time.time() - LAST_EVENT)
     return time.time() - LAST_EVENT < BUTTON_BOUNCE_GAP
 
+def next_track():
+    mpc('play')
+    mpc('next')
+
+def prev_track():
+    mpc('prev')
+    
 def button_callback(channel):
     global LAST_EVENT
     if not(button_bounce()):
         print("Pressed! %s" % channel)
         LAST_EVENT = time.time()
-        if channel == 10: play_next_mp3()
+        if channel in BUTTON_FN:
+            BUTTON_FN[channel]()
     else:
-        print("Bounce")
+        print("Bounce detected")
         
 set_up_playlist()
 
-IO.add_event_detect(10, IO.RISING, callback=button_callback)
+BUTTON_FN = {
+    10: next_track,
+    8: prev_track,
+}
+
+IO.setmode(IO.BOARD)
+for b in BUTTON_FN.keys():
+    IO.setup(b, IO.IN, pull_up_down=IO.PUD_DOWN)
+    IO.add_event_detect(b, IO.RISING, callback=button_callback)
 
 a = raw_input("Press enter to quit\n\n")
 IO.cleanup()
